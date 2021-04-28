dc455d29041763bd4c34bbc82f22098d087abc4f
Roberto Rocchetta <roberto.rocchetta88@gmail.com>
2021-04-28 19:40:24 +0200

    Add files via upload

--- SoftConstrainedScenarioRBDO/G_functions/G_fun_lateralmotioncontroller.m
+++ SoftConstrainedScenarioRBDO/G_functions/G_fun_lateralmotioncontroller.m
@@ -3,12 +3,21 @@ function g=G_fun_lateralmotioncontroller(d,delta)
 % Refer to the article:
 % [1] "Probabilistic and Randomized Tools for Control Design"
 % DOI: 10.1201/b10384-76 Fabrizio Dabbene Fabrizio et al
- 
-% Uncertain aircraft parameters:    Lp   L?   Lr  g/V  Y?   N?   Np   N?   Nr   L?a  Y?r   N?r   N?a
-% In this work:     delta (1)  (2)  (3)  (4)  (5)  (6)  (7) (8)  (9)  (10)  (11)  (12)  (13)      
-% Mean values  -2.93 -4.75 0.78 0.086 -0.11 0.1 -0.042 2.601 -0.29 -3.91 0.035 -2.5335 0.31
+ % see also
+    % https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9198073 
+% Uncertain aircraft parameters:    Lp   L?   Lr  g/V  Y?   N?   Np   N?   Nr    La  Yr   Nr   Na 
+% In this work:              delta (1)  (2)  (3)  (4)   (5)  (6)  (7)   (8)    (9)  (10)  (11)    (12)  (13)      
+% Mean values                     -2.93 -4.75 0.78 0.086 -0.11 0.1 -0.042 2.601 -0.29 -3.91 0.035 -2.5335 0.31
 % Perturbated of 10% arround the mean
  
+% Lp =
+% L?
+% Lr
+% g/V
+% Y
+% N N Nr
+% La  Yr   Nr   Na 
+
 %  Design variables W and P (controller matrices)
 %  Pseq = [0.3075 -0.3164 -0.0973 -0.0188
 %        -0.3164 0.5822 -0.0703 -0.0993
@@ -28,7 +37,7 @@ function g=G_fun_lateralmotioncontroller(d,delta)
 % In this script it correspond to a desing d_opt [Scenario design]:
 %   d_opt=[0.1445 -0.0728 0.0035 0.0085 -0.0728 0.2192 -0.0078 -0.0174 ...
 %         0.0035 -0.0078 0.1375 0.0604 0.0085 -0.0174 0.0604 0.1975 ...
-%       0.0109 0.0908 7.2929 3.4846 0.0439 -0.0565 0.6087 -3.9182]
+%       0.0109 0.0908 7.2929 3.4846 0.0439 -0.0565 0.6087 s-3.9182]
 
  
 % State-space equation is:
@@ -44,7 +53,7 @@ function g=G_fun_lateralmotioncontroller(d,delta)
 % this means that the controller robustly stabilizes the system guaranteeing a desired decay rate ? > 0
 
 % A quadratic performance criterion for this condition is  [2]
-% f(W,P,delta)= A(delta)*P + P*A'(delta) + B(delta)*W' + W*B'(delta) + 2*?*P 
+% f(W,P,delta) =  A(delta)*P + P*A'(delta) + B(delta)*W' + W*B'(delta) + 2*?*P 
 % if G(W,P,delta) is negative semidefinite the condition for stability is sattisified
 % see [2] S. Boyd, L. El Ghaoui, E. Feron, and V. Balakrishnan. Linear Matrix Inequalities in System and Control Theory. SIAM, Philadelphia, 1994
 
@@ -52,31 +61,35 @@ function g=G_fun_lateralmotioncontroller(d,delta)
 % \min\limits_{P \succ 0, W} \{ Tr[W] : P \succeq \thetaI, \max\lambda(f(W,P,delta) \preceq 0 \}
 %where P \in \mathbb{R}^{4\times 4} and W \in \mathbb{R}^{2\times 4}. \theta= 0.01 is a small positive number to ensure the positive de?niteness of P. The control gain K can be ?nally computed as K = Y P?1
 %% START
-P= [d(1:4); 
-    d(5:8); 
-    d(9:12);
-    d(13:16)];
+% P = [d(1:4); 
+%     d(5:8); 
+%     d(9:12);
+%     d(13:16)]; 
+P=[d(1) d(2) d(3) d(4); 
+   d(2) d(5) d(6) d(7); 
+   d(3) d(6) d(8) d(9);
+   d(4) d(7) d(9) d(10)];
 % where P must be positive definite....  check if all(eig(P)>0) ?
-W= [d(17:18);
-    d(19:20);
-    d(21:22); 
-    d(23:24)];
+W = [d(11:12);
+    d(13:14);
+    d(15:16); 
+    d(17:18)];
 
 alpha=0.1; % [3] A Posteriori Probabilistic Bounds of Convex Scenario Programs with Validation Tests Chao Shang et al 7 Jan 2020 arXiv:1903.11734v2 
 g=zeros(size(delta,1),1);
-parfor i=1:size(delta,1)
-    A=[0    ,  1     ,    0       ,    0
-       0 ,delta(i,1)         ,delta(i,2) , delta(i,3)
-       delta(i,4)            ,   0       , delta(i,5)                       ,   -1
+for i=1:size(delta,1)
+    A=[0      ,  1           ,    0          ,     0
+       0      , delta(i,1) , delta(i,2) , delta(i,3)
+       delta(i,4)       ,   0        , delta(i,5)      ,   -1
        delta(i,6)*delta(i,4) , delta(i,7), delta(i,6)+delta(i,6)*delta(i,5), delta(i,9)-delta(i,8)];
 
-    B=[ 0   ,  0
-        0         ,  delta(i,10)
-        delta(i,11) ,   0
+    B=[ 0            ,  0
+        0            ,  delta(i,10)
+        delta(i,11)  ,   0
         delta(i,12)+delta(i,8)*delta(i,11) , delta(i,13)];
     
     
-    f= (A*P+P*A'+B*W'+W*B'+2*alpha*P); %  quadratic performance criterion
+    f= (A*P+P*A.'+B*W.'+W*B.'+2*alpha*P); %  quadratic performance criterion
     %G=10^3-(A*P+P*A'+B*Y'+Y*B'+2*alpha*P);
     g(i,1)=max(eig(f));
     %g(i,2)=max(eig(P));
